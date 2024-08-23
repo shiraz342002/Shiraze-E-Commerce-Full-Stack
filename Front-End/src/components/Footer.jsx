23f7904ced3e9b12bac56664bcd4b6d250acd259
Shiraz Mazhar <imshiraz007@gmail.com>
2024-08-23 14:41:02 +0500

    Home Page Completed

--- Front-End/src/components/Footer.jsx
+++ Front-End/src/components/Footer.jsx
@@ -3,14 +3,14 @@ import { assets } from '../assets/assets'
 const Footer = () => {
   return (
     <div>
-   <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
-    <div >
-        <img src={assets.shiraz_logo} className='mb-5 w-32' alt="" />
-        <p className='w-full md:w-2/3 text-gray-600'>
-        Discover a world of premium fabrics at Shiraz-Fabrics, your ultimate online destination for quality textiles. We offer a curated selection of luxurious fabrics, from elegant silks and rich velvets to durable cottons and contemporary blends.
+   <div className='flex flex-col sm:grid grid-cols-[1fr_1fr_1fr] gap-20  text-sm'>
+    <div className='sm:relative sm:pb-16'>
+        <img src={assets.shiraz_logo} className=' w-24' alt="" />
+        <p className='w-full md:w-2/3 font-medium text-gray-600'>
+        Discover a world of premium fabrics at Shiraz-Fabrics, your ultimate online destination for quality textiles. 
         </p>
     </div>
-    <div>
+    <div className=''>
         <p className='text-xl font-medium mb-5'>Company</p>
         <ul className='flex flex-col gap-1 text-grat-600'>
             <li>Home</li>
@@ -19,6 +19,18 @@ const Footer = () => {
             <li>Privacy Policy</li>
         </ul>
     </div>
+    <div>
+    <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+    <ul className='flex flex-col gap-1 text-grat-600'>
+            <li>0302323269</li>
+            <li>imshiraz007@gmail.com</li>
+            <li>@ShirazFabric</li>
+        </ul>
+    </div>
+  </div>
+  <div>
+    <hr />
+    <p className='py-5 text-sm text-center'>Copyright 2024@ ShirazFabric - All Right Reserved.</p>
   </div>
  </div>
   )
