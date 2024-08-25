f53d4eae4dc1cb3f4a65c94d5b10eea91d9102e2
Shiraz Mazhar <imshiraz007@gmail.com>
2024-08-25 18:05:04 +0500

    Delivery-Form-Made

--- Front-End/src/Pages/PlaceOrder.jsx
+++ Front-End/src/Pages/PlaceOrder.jsx
@@ -1,9 +1,56 @@
 import React from 'react'
+import Title from '../components/Title'
+import CartTotal from '../components/CartTotal'
+import { assets } from '../assets/assets'
 
 const PlaceOrder = () => {
   return (
-    <div>
-      
+    <div className='flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t'>
+    <div className='flex flex-col gap-4 w-full sm:max-w-[480px]'>
+        {/* //left side  */}
+        <div className='text-xl sm:text-2xl my-3'>
+          <Title heading1={"DELIVERY"}  heading2={"INFORMATION"}/>
+        </div>
+        <div className='flex gap-3'>
+            <input className='border border-gray-200 rounded py-1.5 px-3.5 w-full' type="text" placeholder='First name' />
+            <input className='border border-gray-200 rounded py-1.5 px-3.5 w-full' type="text" placeholder='Last name' />
+        </div>
+            <input className='border border-gray-200 rounded py-1.5 px-3.5 w-full' type="email" placeholder='Email address' />
+            <input className='border border-gray-200 rounded py-1.5 px-3.5 w-full' type="text" placeholder='Street' />
+          <div className='flex gap-3'>
+            <input className='border border-gray-200 rounded py-1.5 px-3.5 w-full' type="text" placeholder='City' />
+            <input className='border border-gray-200 rounded py-1.5 px-3.5 w-full' type="text" placeholder='State' />
+        </div>
+          <div className='flex gap-3'>
+            <input className='border border-gray-200 rounded py-1.5 px-3.5 w-full' type="number" placeholder='Zipcode' />
+            <input className='border border-gray-200 rounded py-1.5 px-3.5 w-full' type="text" placeholder='Country' />
+        </div>
+            <input className='border border-gray-200 rounded py-1.5 px-3.5 w-full' type="number" placeholder='Phone' />
+    </div>
+
+    {/* //Right Side */}
+    <div className='mt-8'>
+      <div className="mt-8 min-w-80">
+        <CartTotal/>
+      </div>
+      <div className="mt-12">
+        <Title heading1={"PAYMENT"} heading2={"METHOD"}/>
+        <div className='flex flex-col lg:flex-row gap-4'>
+            <div className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
+                <p className={`min-w-3.5 h-3.5 border rounded-full`}></p>
+                <img className='h-5 mx-4' src={assets.stripe_logo} alt="" />
+            </div>
+            <div className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
+                <p className={`min-w-3.5 h-3.5 border rounded-full`}></p>
+                <img className='h-5 mx-4' src={assets.razorpay_logo} alt="" />
+            </div>
+            <div className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
+                <p className={`min-w-3.5 h-3.5 border rounded-full`}></p>
+                <p className='text-gray-500 text-sm font-medium mx-4'>CASH ON DELIVERY</p>
+            </div>
+        </div>
+      </div>
+    </div>
     </div>
   )
 }
--- Front-End/src/main.jsx
+++ Front-End/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import StoreContextProvider from "./Store/StoreContext.jsx"
-// https://youtu.be/ZF73dpgRrWI?t=15485
+// https://youtu.be/ZF73dpgRrWI?t=16138
 // https://forever-ecom.vercel.app/
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
