3d8074d3c7245fc30f548203911656162c5e037b
Shiraz Mazhar <imshiraz007@gmail.com>
2024-08-25 23:19:06 +0500

    Payment Method Fixed

--- Front-End/src/Pages/PlaceOrder.jsx
+++ Front-End/src/Pages/PlaceOrder.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Title from '../components/Title'
 import CartTotal from '../components/CartTotal'
 import { assets } from '../assets/assets'
 
 const PlaceOrder = () => {
+
+  const [paymentMethod,setPaymentMethod]=useState('cod')
+  useEffect(()=>{
+    console.log(paymentMethod);
+
+  },[paymentMethod])
   return (
     <div className='flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t'>
     <div className='flex flex-col gap-4 w-full sm:max-w-[480px]'>
@@ -30,24 +36,24 @@ const PlaceOrder = () => {
 
     {/* //Right Side */}
     <div className='mt-8'>
-      <div className="mt-8 min-w-80">
+      <div className="mt-8 min-w-80"> 
         <CartTotal/>
       </div>
       <div className="mt-12">
         <Title heading1={"PAYMENT"} heading2={"METHOD"}/>
-        <div className='flex flex-col lg:flex-row gap-4'>
+        <div onClick={()=>setPaymentMethod('stripe')} className='flex flex-col lg:flex-row gap-4'>
             <div className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
-                <p className={`min-w-3.5 h-3.5 border rounded-full`}></p>
+                <p className={`min-w-3.5 h-3.5 border rounded-full ${paymentMethod ==='stripe'? 'bg-green-400':''}`}></p>
                 <img className='h-5 mx-4' src={assets.stripe_logo} alt="" />
             </div>
-            <div className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
-                <p className={`min-w-3.5 h-3.5 border rounded-full`}></p>
+            </div>
+            <div onClick={()=>setPaymentMethod('razorpay')} className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
+                <p className={`min-w-3.5 h-3.5 border rounded-full ${paymentMethod === 'razorpay'? 'bg-green-400':''}`}></p>
                 <img className='h-5 mx-4' src={assets.razorpay_logo} alt="" />
             </div>
-            <div className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
-                <p className={`min-w-3.5 h-3.5 border rounded-full`}></p>
+            <div onClick={()=>setPaymentMethod('cod')} className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
+                <p className={`min-w-3.5 h-3.5 border rounded-full ${paymentMethod === 'cod'? 'bg-green-400':''}`}></p>
                 <p className='text-gray-500 text-sm font-medium mx-4'>CASH ON DELIVERY</p>
-            </div>
         </div>
       </div>
     </div>
