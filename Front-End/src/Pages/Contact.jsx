6efdcd086e724d4ba6899befdf6ea52d8dad056f
Shiraz Mazhar <imshiraz007@gmail.com>
2024-08-27 11:45:08 +0500

    Created Contact Page

--- Front-End/src/Pages/Contact.jsx
+++ Front-End/src/Pages/Contact.jsx
@@ -9,16 +9,20 @@ const Contact = () => {
             <Title heading1={"CONTACT"} heading2={"US"}/>
         </div>
         </div>
-        <div className="my-10 flex flex-col md:flex-row gap-10 mb-28">
+        <div className="my-10 flex flex-col justify-center md:flex-row gap-14  mb-28">
             <div>
                 <img className='ml-4 w-full md:max-w-[480px]' src={assets.contact_img} alt="" />
             </div>
             <div className='flex flex-col gap-5 justify-center items-start'>
-              <p className='font-bold text-2xl'>Our Store</p>
-                <p>54709 Willms StationSuite 350, Washington, USA</p>
+              <p className ='font-semibold text-2xl'>Our Store</p>
+                <p>54709 Willms StationSuite <br /> 350, Washington, USA</p>
                 <p>Tel: (415) 555-0132
-
+                <br />
+                Email: admin@forever.com
                 </p>
+                <p className='font-semibold text-2xl'>Careers at Shiraz Fabrics</p>
+                <p>Learn more about our teams and job openings.</p>
+                <button className='py-4 px-7 border border-black font-medium text-sm hover:bg-black hover:text-white transition ease-out duration-400 transform hover:scale-105'>Explore Jobs</button>
             </div>
         </div>
     </div>
