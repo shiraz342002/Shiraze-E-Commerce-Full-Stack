0fdb0d3aa7008679f3ce47ade6c13be4aa42c197
Shiraz Mazhar <92252886+shiraz342002@users.noreply.github.com>
2024-08-29 17:14:49 +0500

    Update main.jsx

--- Front-End/src/main.jsx
+++ Front-End/src/main.jsx
@@ -3,8 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import StoreContextProvider from "./Store/StoreContext.jsx"
-// https://youtu.be/ZF73dpgRrWI?t=16459
-// https://forever-ecom.vercel.app/
+
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <StoreContextProvider>
